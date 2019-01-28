dcd14d3549bdc91226d525b272cbf5bc0963b427
Jessica Zhu <jfzhu@mit.edu>
2019-01-27 21:38:52 -0800

    fixed stuff i broke so now adding players to rooms works properly

--- public/js/room.js
+++ public/js/room.js
@@ -1,6 +1,6 @@
-var Tractor = require('../../Tractor.js');
-var Deck = require('../../Deck.js');
-var Game = require('../../Game.js');
+// var Tractor = require('../../Tractor.js');
+// var Deck = require('../../Deck.js');
+// var Game = require('../../Game.js');
 var socket = io();
 
 socket.emit('joined game room', {'gameid':gameid, 'username':username});
@@ -10,14 +10,14 @@ socket.emit('joined game room', {'gameid':gameid, 'username':username});
 
 // called whenever a player clicks the deal card button (button not made yet)
 function dealCardNotify() {
-  socket.emit('deal card');
+  socket.emit('deal card', player);
 }
 
 function chooseTrumpNotify() {
   socket.emit('player chose trump', {'player': player, 'trumpSuit': trumpSuit});
 }
 
-function showDealCardButton(player) {
+function showDealCardButton() {
   // logic here is prob wrong
   $("body").append("<button type='button' id='dealCardButton'>Deal Card</button>");
   $("Deal Card").click(dealCardNotify);
@@ -42,7 +42,7 @@ $(function () {
       players.append("<li id=\"" + username + "\">" + username + "</li>");
     }
 
-    if (username == owner.username) {
+    if (username == owner) {
       $('#title').text($('#title').text() + " (Owner)");
     }
   });
@@ -75,55 +75,32 @@ $(function () {
   socket.on('closed room', function() {
     // SOME LOGIC THAT TAKES YOU BACK TO THE LOBBY
   });
-
-  socket.on('start game', function(tempDetails) {
-    var gameId = tempDetails[gameId];
-    var currentPlayers = tempDetails[currentPlayers];
-    console.log("Creating game " + gameId);
-    var tractor = new Tractor(gameId);
-    console.log("Adding in " + currentPlayers.length + " players to game");
-    for (var i = 0; i < currentPlayers.length; i++) {
-      tractor.addPlayer(currentPlayers[i]);
-    }
-    var game = new Game(currentPlayers);
-    // once tractor game is initialized, begin dealing of cards
-    var deck = new Deck(2);
-    deck.shuffle();
-    while (deck.length != deck.VAULT_SIZE()) {
-      for (var i = 0; i < currentPlayers.length; i++) {
-        let player = currentPlayers[i];
-        showDealCardButton(player); // function that allowed player to click button
-        socket.on('deal card', function() {
-          game.dealCard(player);
-        });
-      }
-    }
-    // after dealing is done, and trump is chosen, let the banker have the vault cards
-    for (let i = 0; i < deck.length; i++) {
-      game.dealCard(game.banker);
-    }
     
+  socket.on('deal card', function(player) {
+    if (socket.id == player.id) {
+      showDealCardButton();
 
-  });
-
-  socket.on('banker chose vault cards', function() {
-    // if the banker put down 8 cards, start game
-    socket.emit('start round', game);
-  })
-
-  socket.on('player chose trump', function(trumpInfo) {
-    var trumpSuit = trumpInfo.suit;
-    var player = trumpInfo.player; // only relevant for the first round
-    game.trumpSuit = trumpSuit;
-    if (firstRound) {
-      game.banker = player;
     }
-
   })
 
-  socket.on('start round', function(game) {
-    // game will be game object
-    // round implementation here
-  });
+  // socket.on('banker chose vault cards', function() {
+  //   // if the banker put down 8 cards, start game
+  //   socket.emit('start round', game);
+  // })
+
+  // socket.on('player chose trump', function(trumpInfo) {
+  //   var trumpSuit = trumpInfo.suit;
+  //   var player = trumpInfo.player; // only relevant for the first round
+  //   game.trumpSuit = trumpSuit;
+  //   if (firstRound) {
+  //     game.banker = player;
+  //   }
+
+  // })
+
+  // socket.on('start round', function(game) {
+  //   // game will be game object
+  //   // round implementation here
+  // });
 
 });
